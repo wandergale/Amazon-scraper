35d8316a60b7973e522853d4717be3ae0ef4684d
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-21 09:51:53 -0300

    add: dotenv & express working

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
