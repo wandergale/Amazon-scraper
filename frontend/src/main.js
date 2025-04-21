04dbf087cfb923a0092b3f3ae26829fbdb723cc5
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-21 12:28:53 -0300

    add: returning results

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,57 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#scraperBtn").addEventListener("click", async () => {
+  const keyword = document.querySelector("#keyword").value.trim();
+  const resultContainer = document.querySelector("#results");
 
-setupCounter(document.querySelector('#counter'))
+  const warning = document.createElement("p");
+  warning.innerHTML = `Loading...`; // warning to user see when the request was send
+  resultContainer.appendChild(warning);
+
+  if (!keyword) {
+    alert("Please enter a keyword!");
+    return;
+  }
+
+  const xhr = new XMLHttpRequest();
+  const url = `http://localhost:3000/api/scrape?keyword=${encodeURIComponent(
+    keyword
+  )}`;
+
+  // method get config and ajax url
+  xhr.open("GET", url, true);
+
+  // when the request complete
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status <= 300) {
+      const data = JSON.parse(xhr.responseText);
+
+      resultContainer.innerHTML = ""; // clean previws results
+
+      data.forEach((product) => {
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
+        productDiv.innerHTML = `
+        <img src=${product.image} alt="${product.title}"/>
+        <div class="productInfo">
+          <h3>${product.title}</h3>
+        </div>
+        `;
+        // clean warning
+        warning.innerHTML = "";
+        resultContainer.appendChild(productDiv);
+      });
+    } else {
+      alert("Error fetching data!");
+    }
+  };
+
+  // error
+  xhr.onerror = () => {
+    alert("Request failed");
+  };
+
+  // send request
+  xhr.send();
+});
